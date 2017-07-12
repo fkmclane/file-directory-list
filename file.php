47e93dd8c46471790b0c80a14ca0e70d07179459
Foster McLane <fkmclane@gmail.com>
2017-07-12 19:22:59 -0400

    bring up to date with upstream 1.9

--- file.php
+++ file.php
@@ -22,19 +22,19 @@ function _file_format_size($file) {
 		return round($bytes/1099511627776, 2) . 'tb';
 }
 
-function _file_display_block(&$output, $file, $file_ext) {
+function _file_display_block(&$output, $file, $file_ext, $download) {
 	$output .= "	<div class=\"block\">\n";
-	$output .= "		<a href=\"$file\" class=\"$file_ext\">\n";
+	$output .= "		<a href=\"$file\" class=\"$file_ext\"" . ($download ? " download=\"" . basename($file) . "\"" : "") . ">\n";
 	$output .= "			<div class=\"img $file_ext\">&nbsp;</div>\n";
 	$output .= "			<div class=\"name\">\n";
 	$output .= "				<div class=\"file\">" . basename($file) . "</div>\n";
-	$output .= "				<div class=\"date\">Size: " . _file_format_size($file) . "<br/>Last modified: " .  date("D. F jS, Y - h:ia", filemtime($file)) . "</div>\n";
+	$output .= "				<div class=\"date\">Size: " . ($file_ext == 'dir' ? "-" : _file_format_size($file)) . "<br/>Last modified: " .  date("D. F jS, Y - h:ia", filemtime($file)) . "</div>\n";
 	$output .= "			</div>\n";
 	$output .= "		</a>\n";
 	$output .= "	</div>\n";
 }
 
-function _file_build_blocks(&$output, $folder, $dir, $root, $sort_by, $sub_folders, $ignore_file_list, $ignore_ext_list) {
+function _file_build_blocks(&$output, $folder, $dir, $root, $sort_by, $sub_folders, $force_download, $ignore_empty_folders, $ignore_file_list, $ignore_ext_list) {
 	$objects = array();
 	$objects['directories'] = array();
 	$objects['files'] = array();
@@ -53,7 +53,7 @@ function _file_build_blocks(&$output, $folder, $dir, $root, $sort_by, $sub_folde
 
 		$real = "$dir/$item";
 
-		$file_ext = _file_get_file_ext($real);
+		$file_ext = is_dir($real) ? 'dir' : _file_get_file_ext($real);
 
 		if (in_array($file_ext, $ignore_ext_list))
 			continue;
@@ -74,12 +74,22 @@ function _file_build_blocks(&$output, $folder, $dir, $root, $sort_by, $sub_folde
 		arsort($objects['directories']);
 
 	foreach ($objects['directories'] as $c => $file) {
-		_file_display_block($output, "$dir/$file", "dir");
+		$dir_output = '';
+		$sub_output = '';
 
-		if ($sub_folders) {
-			$output .= "<div class=\"sub\" data-folder=\"$root/$file\">\n";
-			_file_build_blocks($output, $file, $dir, $root, $sort_by, $sub_folders, $ignore_file_list, $ignore_ext_list);
-			$output .= "</div>\n";
+		_file_display_block($dir_output, "$dir/$file", "dir", false);
+
+		if ($sub_folders)
+			_file_build_blocks($sub_output, $file, $dir, $root, $sort_by, $sub_folders, $force_download, $ignore_empty_folders, $ignore_file_list, $ignore_ext_list);
+
+		if (!$ignore_empty_folders || !$sub_folders || $sub_output !== '') {
+			$output .= $dir_output;
+
+			if ($sub_folders) {
+				$output .= "<div class=\"sub\" data-folder=\"$root/$file\">\n";
+				$output .= $sub_output;
+				$output .= "</div>\n";
+			}
 		}
 	}
 
@@ -95,11 +105,11 @@ function _file_build_blocks(&$output, $folder, $dir, $root, $sort_by, $sub_folde
 	foreach ($objects['files'] as $t => $file) {
 		$file_ext = _file_get_file_ext("$dir/$file");
 
-		_file_display_block($output, "$root/$file", $file_ext);
+		_file_display_block($output, "$root/$file", $file_ext, $force_download);
 	}
 }
 
-function file_head($jquery=true, $font=true, $css="https://raw.githubusercontent.com/fkmclane/php-file/master/file.css") {
+function file_head($jquery=true, $font=true, $css="https://gitcdn.link/repo/fkmclane/php-file/master/file.css") {
 	$output = "";
 
 	if ($jquery)
@@ -110,12 +120,12 @@ function file_head($jquery=true, $font=true, $css="https://raw.githubusercontent
 
 	$output .= "<link href=\"$css\" rel=\"stylesheet\" type=\"text/css\"/>";
 
-	$output .= "<script>$(document).ready(function() { $(\"a.dir\").click(function(e) { $('.sub[data-folder=\"' + $(this).attr('href') + '\"]').slideToggle(); e.preventDefault(); }); });</script>\n";
+	$output .= "<script>$(document).ready(function() { $(\".php-file.sub-folders a.dir\").click(function(e) { $('.sub[data-folder=\"' + $(this).attr('href') + '\"]').slideToggle(); e.preventDefault(); }); });</script>\n";
 
 	return $output;
 }
 
-function file_list($dir, $root=false, $title=false, $sort_by='name_asc', $sub_folders=true, $ignore_file_list=array('.htaccess', 'Thumbs.db', '.DS_Store', 'index.php'), $ignore_ext_list=array()) {
+function file_list($dir, $root=false, $title=false, $sort_by='name_asc', $sub_folders=true, $force_download=true, $ignore_empty_folders=true, $ignore_file_list=array('.htaccess', 'Thumbs.db', '.DS_Store', 'index.php'), $ignore_ext_list=array()) {
 	if (!$title)
 		$title = _file_clean_title(basename($dir));
 
@@ -124,9 +134,9 @@ function file_list($dir, $root=false, $title=false, $sort_by='name_asc', $sub_fo
 
 	$output = "";
 
-	$output .= "<div class=\"php-file\">\n<h1>$title</h1>\n<div class=\"wrap\">\n";
+	$output .= "<div class=\"php-file" . ($sub_folders ? " sub-folders" : "") . "\">\n<h1>$title</h1>\n<div class=\"wrap\">\n";
 
-	_file_build_blocks($output, false, $dir, $root, $sort_by, $sub_folders, $ignore_file_list, $ignore_ext_list);
+	_file_build_blocks($output, false, $dir, $root, $sort_by, $sub_folders, $force_download, $ignore_empty_folders, $ignore_file_list, $ignore_ext_list);
 
 	$output .= "</div>\n</div>\n";
 
